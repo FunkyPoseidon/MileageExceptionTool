ff444bd92a49e64f2c2bfbe3e6e18690322a2437
Isaac Campbell <jiqcrock@gmail.com>
2026-07-15 12:05:55 -0500

    Add startup crash logging

--- src/main/java/com/tdem/milesheetcreator/Launcher.java
+++ src/main/java/com/tdem/milesheetcreator/Launcher.java
@@ -1,8 +1,31 @@
 package com.tdem.milesheetcreator;
 
+import java.io.File;
+import java.io.PrintWriter;
+
 public class Launcher {
 
     public static void main(String[] args) {
-        MainApplication.main(args);
+
+        try {
+            MainApplication.main(args);
+
+        } catch (Throwable t) {
+
+            try {
+                File log = new File(
+                        System.getProperty("user.home"),
+                        "MileageExceptionTool-error.txt"
+                );
+
+                PrintWriter writer = new PrintWriter(log);
+                t.printStackTrace(writer);
+                writer.close();
+
+            } catch (Exception ignored) {
+            }
+
+            throw t;
+        }
     }
 }
